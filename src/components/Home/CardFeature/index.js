d4ac150f0a5d0615fc562a8603c8e33d085fe75e
Muhammad Zulfahmi Sadrah <zulfahmisadrah@gmail.com>
2021-09-24 19:10:17 +0800

    update card backround and button color

--- src/components/Home/CardFeature/index.js
+++ src/components/Home/CardFeature/index.js
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 export default function CardFeatures(props) {
   return (
     <Col>
-      <Card style={{ height: "max-content", marginTop: 24, backgroundColor:"#F9FEFB"}}>
+      <Card style={{ height: "max-content", marginTop: 24, backgroundColor:"#FFFFFF"}}>
         <Card.Img variant="top" src={props.img} className="p-4" style={{height: 230}} />
         <Card.Body
           className="px-4"
@@ -31,7 +31,7 @@ export default function CardFeatures(props) {
           >
             {props.desc}
           </Card.Text>
-          <Link to='/Signin' className='btn btn-primary' style={{ textDecoration: 'none' }}>Sign In</Link>
+          <Link to='/Signin' className='btn btn-primary' style={{ textDecoration: 'none', backgroundColor: '#125D98' }}>Sign In</Link>
         </Card.Body>
       </Card>
     </Col>
